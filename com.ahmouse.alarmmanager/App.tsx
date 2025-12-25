b9ed2b9e9be175d3846bc35b8b369a46c1ddd97c
Ahmed A <as.abdulla@proton.me>
2025-12-25 13:10:00 -0600

    Initial app setup

--- com.ahmouse.alarmmanager/App.tsx
+++ com.ahmouse.alarmmanager/App.tsx
@@ -1,38 +1,61 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {AlarmListItem} from './components/alarm';
-import { Appbar } from 'react-native-paper';
+import {AlarmList} from './components/alarm';
+import { Appbar, FAB, Portal, useTheme } from 'react-native-paper';
 
 export default function App() {
-  let time = new Date(1980, 1, 5, 10, 30);
+  const theme = useTheme();
+
+  const styles = StyleSheet.create({
+    rootContainer: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: theme.colors.primaryContainer,
+    },
+    container: {
+      flex: 1,
+      backgroundColor: theme.colors.primaryContainer,
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      marginTop: 20
+    },
+    appbar: {
+
+    },
+    addButton: {
+      position: 'absolute',
+      right: '10%',
+      bottom: '10%',
+    }
+  });
+
+  let alarms = [
+    {
+      id: '1',
+      time: new Date(1980, 1, 5, 10, 30)
+    },
+    {
+      id: '2',
+      time: new Date(1980, 1, 5, 12, 23)
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-
-      <StatusBar
-        backgroundColor="red"
-      />
-
+    <View style={styles.rootContainer}>
+      <StatusBar/>
       <Appbar.Header style={styles.appbar}>
         <Appbar.Content title="Alarms" />
-        <Appbar.Action icon="dots" onPress={() => {}} />
+        <Appbar.Action icon="menu" onPress={() => {}} />
       </Appbar.Header>
 
-      <AlarmListItem time={time}/>
-
+      <AlarmList items={alarms}/>
+      <Portal>
+        <FAB icon="plus" style={styles.addButton}/>
+      </Portal>
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    marginTop: 20
-  },
-  appbar: {
 
-  }
-});
